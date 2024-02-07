e99f842f1f008def454f608306015eac222d0b65
wojciechdrabczyk <wojciechdrabczyk@icloud.com>
2024-02-07 11:56:52 +0100

    refactor: search button

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Category;
-use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
@@ -17,25 +17,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    $posts = Post::latest();
-
-    if (request('search')) {
-        $posts->where('title', 'like', '%' . request('search') . '%')
-            ->orWhere('body', 'like', '%' . request('search') . '%');
-    };
-    return view('posts', [
-        'posts' => $posts->get(),
-        'categories' => Category::all(),
-    ]);
-})->name('home');
-Route::get('posts/{post:slug}', function (Post $post) {
-    //    $post->load('author');
-    //    ddd($post->load('author')->toSql());
-    return view('post', [
-        'post' => $post,
-    ]);
-});
+Route::get('/', [PostController::class, 'index'])->name('home');
+Route::get('posts/{post:slug}', [PostController::class, "show"]);
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
