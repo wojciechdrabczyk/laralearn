6b93ea7760c595a7f4bf3997653cedbe21a65a7e
wojciechdrabczyk <wojciechdrabczyk@icloud.com>
2024-02-12 17:39:14 +0100

    feat: pagination

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,6 +3,7 @@
     <main class="mx-auto mt-6 max-w-6xl space-y-6 lg:mt-20">
         @if ($posts->count())
             <x-posts-grid :posts="$posts" />
+            {{$posts->links()}}
         @else
             <p class="text-center">No posts yet, please check back later.</p>
         @endif
