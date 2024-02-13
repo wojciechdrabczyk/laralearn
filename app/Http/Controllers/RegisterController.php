8439e5d3cc8dd49a7fcd50853cb93814affa6342
wojciechdrabczyk <wojciechdrabczyk@icloud.com>
2024-02-13 19:31:20 +0100

    feat: register user page

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+class RegisterController extends Controller
+{
+    public function create()
+    {
+        return view('register.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'name' => 'required',
+            'username' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'password' => ['required', 'min:7', 'max:255']
+        ]);
+        User::create($attributes);
+        return redirect('/');
+    }
+}
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,17 +2,17 @@
 
 <title>Laravel From Scratch Blog</title>
 <link
-    href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
-    rel="stylesheet"
+        href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
+        rel="stylesheet"
 />
-<link rel="preconnect" href="https://fonts.gstatic.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" />
 <link
-    href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap"
-    rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap"
+        rel="stylesheet"
 />
 <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 <body
-    style="
+        style="
         font-family:
             Open Sans,
             sans-serif;
@@ -23,10 +23,10 @@
         <div>
             <a href="/">
                 <img
-                    src="/images/logo.svg"
-                    alt="Laracasts Logo"
-                    width="165"
-                    height="16"
+                        src="/images/logo.svg"
+                        alt="Laracasts Logo"
+                        width="165"
+                        height="16"
                 />
             </a>
         </div>
@@ -35,8 +35,8 @@
             <a href="/" class="text-xs font-bold uppercase">Home Page</a>
 
             <a
-                href="#"
-                class="ml-3 rounded-full bg-blue-500 px-5 py-3 text-xs font-semibold uppercase text-white"
+                    href="#"
+                    class="ml-3 rounded-full bg-blue-500 px-5 py-3 text-xs font-semibold uppercase text-white"
             >
                 Subscribe for Updates
             </a>
@@ -46,13 +46,13 @@
     {{ $slot }}
 
     <footer
-        class="mt-16 rounded-xl border border-black border-opacity-5 bg-gray-100 px-10 py-16 text-center"
+            class="mt-16 rounded-xl border border-black border-opacity-5 bg-gray-100 px-10 py-16 text-center"
     >
         <img
-            src="/images/lary-newsletter-icon.svg"
-            alt=""
-            class="mx-auto -mb-6"
-            style="width: 145px"
+                src="/images/lary-newsletter-icon.svg"
+                alt=""
+                class="mx-auto -mb-6"
+                style="width: 145px"
         />
         <h5 class="text-3xl">Stay in touch with the latest posts</h5>
         <p class="mt-3 text-sm">
@@ -61,28 +61,28 @@
 
         <div class="mt-10">
             <div
-                class="relative mx-auto inline-block rounded-full lg:bg-gray-200"
+                    class="relative mx-auto inline-block rounded-full lg:bg-gray-200"
             >
                 <form method="POST" action="#" class="text-sm lg:flex">
                     <div class="flex items-center lg:px-5 lg:py-3">
                         <label for="email" class="hidden lg:inline-block">
                             <img
-                                src="/images/mailbox-icon.svg"
-                                alt="mailbox letter"
+                                    src="/images/mailbox-icon.svg"
+                                    alt="mailbox letter"
                             />
                         </label>
 
                         <input
-                            id="email"
-                            type="text"
-                            placeholder="Your email address"
-                            class="py-2 pl-4 focus-within:outline-none lg:bg-transparent lg:py-0"
+                                id="email"
+                                type="text"
+                                placeholder="Your email address"
+                                class="py-2 pl-4 focus-within:outline-none lg:bg-transparent lg:py-0"
                         />
                     </div>
 
                     <button
-                        type="submit"
-                        class="mt-4 rounded-full bg-blue-500 px-8 py-3 text-xs font-semibold uppercase text-white transition-colors duration-300 hover:bg-blue-600 lg:ml-3 lg:mt-0"
+                            type="submit"
+                            class="mt-4 rounded-full bg-blue-500 px-8 py-3 text-xs font-semibold uppercase text-white transition-colors duration-300 hover:bg-blue-600 lg:ml-3 lg:mt-0"
                     >
                         Subscribe
                     </button>
