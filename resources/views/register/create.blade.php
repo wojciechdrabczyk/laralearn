001257a615b86b620bc59b0cf22fc443d05018d5
wojciechdrabczyk <wojciechdrabczyk@icloud.com>
2024-02-14 18:45:12 +0100

    feat: error messages on register page

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -15,8 +15,12 @@
                            type="text"
                            name="name"
                            id="name"
+                           value="{{old('name')}}"
                            required
                     >
+                    @error('name')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -30,8 +34,12 @@
                            type="text"
                            name="username"
                            id="username"
+                           value="{{old('username')}}"
                            required
                     >
+                    @error('username')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
                 </div>
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
@@ -41,11 +49,15 @@
                     </label>
 
                     <input class="border border-gray-400 p-2 w-full"
-                           type="text"
+                           type="email"
                            name="email"
                            id="email"
+                           value="{{old('email')}}"
                            required
                     >
+                    @error('email')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
                 </div>
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
@@ -60,6 +72,9 @@
                            id="password"
                            required
                     >
+                    @error('password')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
                 </div>
                 <div class="mb-6">
                     <button type="submit"
