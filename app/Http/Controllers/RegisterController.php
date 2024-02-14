6f9d8eb0c6cb8bca91109567c4bb9c1346e6ec62
wojciechdrabczyk <wojciechdrabczyk@icloud.com>
2024-02-14 18:44:47 +0100

    feat: session flash

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Validation\Rule;
 
 class RegisterController extends Controller
 {
@@ -20,6 +21,6 @@ class RegisterController extends Controller
             'password' => ['required', 'min:7', 'max:255']
         ]);
         User::create($attributes);
-        return redirect('/');
+        return redirect('/')->with('success', 'Your account has been created.');
     }
 }
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -91,4 +91,5 @@
         </div>
     </footer>
 </section>
+<x-flash/>
 </body>
