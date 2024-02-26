b57067bfa1ca99177407a4d6cc3fea3be808b2de
wojciechdrabczyk <wojtek@lycal.dev>
2024-02-26 13:19:47 +0100

    refactor: extracted Mailchimp component and made it more generic

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,17 +2,26 @@
 
 namespace App\Providers;
 
+use App\Services\MailchimpNewsletter;
+use App\Services\Newsletter;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use MailchimpMarketing\ApiClient;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    public function register(): void
+    public function register()
     {
-        //
+        app()->bind(Newsletter::class, function () {
+            $client = (new ApiClient)->setConfig([
+                'apiKey' => config('services.mailchimp.key'),
+                'server' => 'us22'
+            ]);
+            return new MailchimpNewsletter($client);
+        });
     }
 
     /**
